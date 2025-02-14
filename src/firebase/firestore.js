bb760169556413f352d8f515f20b8e4f136374a9
yuvraj-rathod-1202 <rathodyuvraj12022007>
2025-02-15 01:51:36 +0530

    add more functions

--- src/firebase/firestore.js
+++ src/firebase/firestore.js
@@ -47,8 +47,22 @@ const onMessageUpdate = (callback) => {
 const setEventData = async (eventData) => {
     try{
         await setDoc(doc(firebase, "events", eventData.id), {
+            uid: eventData.id,
             event: eventData.event,
+            booked: [],
+            redeemed: [],
             sender: eventData.sender,
+        });
+        const userdoc = doc(firebase, "users");
+        const q = query(userdoc, where("uid", "==", eventData.sender));
+        const doc = await getDoc(q);
+        const createdEvents = doc.data().createdEvents;
+        const contributions = doc.data().contributions;
+        createdEvents.push(eventData.id);
+        contributions+=1;
+        await setDoc(doc, {
+            createdEvents: createdEvents,
+            contributions: contributions,
         });
     }catch(e){
         console.error("Error adding document: ", e);
@@ -92,10 +106,25 @@ const getImageData = async (imageName) => {
 }
 
 const setImageData = async (imageData) => {
-    await setDoc(doc(firebase, "imageData", imageData.id), {
-        name: imageData.name,
-        sender: imageData.sender,
-    });
+    try{
+        await setDoc(doc(firebase, "imageData", imageData.id), {
+            name: imageData.name,
+            sender: imageData.sender,
+        });
+        const userdoc = doc(firebase, "users");
+        const q = query(userdoc, where("uid", "==", imageData.sender));
+        const doc = await getDoc(q);
+        const createdImages = doc.data().createdImages;
+        const contributions = doc.data().contributions;
+        createdImages.push(imageData.id);
+        contributions+=1;
+        await setDoc(doc, {
+            createdImages: createdImages,
+            contributions: contributions,
+        });
+    }catch(e){
+        console.error("Error adding document: ", e);
+    }
 }
 
 const fetchImageData = async () => {
@@ -122,7 +151,40 @@ const eventBookings = async (eventData) => {
         await setDoc(doc, {
             participantedEvents: participantedEvents,
         });
+
+        const eventdoc = doc(firebase, "events");
+        const q2 = query(eventdoc, where("uid", "==", eventData.id));
+        const doc2 = await getDoc(q2);
+        const booked = doc2.data().booked;
+        booked.push(eventData.booker);
+        await setDoc(doc2, {
+            booked: booked,
+        });
     }catch(e){
         console.error("Error adding document: ", e);
     }
 }
+
+const eventBookingsCheck = async (eventData) => {
+    const userdoc = doc(firebase, "users");
+    const q = query(userdoc, where("uid", "==", eventData.booker));
+    const doc = await getDoc(q);
+    const participantedEvents = doc.data().participantedEvents;
+    if(participantedEvents.includes(eventData.id)){
+        participantedEvents.remove(eventData.id);
+        await setDoc(doc, {
+            participantedEvents: participantedEvents,
+        });
+
+        const eventdoc = doc(firebase, "events");
+        const q2 = query(eventdoc, where("uid", "==", eventData.id));
+        const doc2 = await getDoc(q2);
+        const booked = doc2.data().booked;
+        booked.push(eventData.booker);
+        await setDoc(doc2, {
+            booked: booked,
+        });
+    }
+}
+
+
